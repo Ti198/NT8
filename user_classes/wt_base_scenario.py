79adf2e705d023499eb1a979e2927f1274461c03
Ti198 <tisanina2000@gmail.com>
2024-11-07 22:14:10 +0300

    сдвинули debug на 36 и 43 стр.

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -15,7 +15,6 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
             self.test_users_data = open_csv_file(self.test_users_csv_filepath)
             logger.info(f"Test data for users is: {self.test_users_data}")
 
-           
             r00_0_headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
             'Accept-Encoding': 'gzip, deflate, br, zstd',
@@ -34,14 +33,14 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
                 '/WebTours/',
                 name="req_00_0_getHomePage_/WebTours/",
                 allow_redirects=False,
-            #debug_stream=sys.stderr
+                #debug_stream=sys.stderr
             )        
 
             self.client.get(
                 '/WebTours/header.html',
                 name="req_00_1_getHomePage_/WebTours/header.html",
                 allow_redirects=False,
-            #debug_stream=sys.stderr
+                #debug_stream=sys.stderr
             )
 
             r_02_url_param_signOff = 'true' 
@@ -50,7 +49,7 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
                 f'/cgi-bin/welcome.pl?signOff={r_02_url_param_signOff}',
                 name="req_00_2_getHomePage_cgi-bin/welcome.pl?signOff",
                 allow_redirects=False,
-            #debug_stream=sys.stderr
+                #debug_stream=sys.stderr
             )
 
             with self.client.get(
@@ -58,7 +57,7 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
                 name="req_00_3_getHomePage_cgi-bin/nav.pl?in=home",
                 allow_redirects=False,
                 catch_response=True
-            #debug_stream=sys.stderr,
+                #debug_stream=sys.stderr,
             ) as req00_3_response:
                 check_http_response(req00_3_response, 'name="userSession"')
             # print(f"\n__\n req_00_3 response text: {req00_3_response.text}\n__\n")
@@ -71,7 +70,7 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
                 f'/WebTours/home.html',
                 name="req_00_4_getHomePage_WebTours/home.html",
                 allow_redirects=False,
-            #debug_stream=sys.stderr
+                #debug_stream=sys.stderr
             )
 
         @task()
